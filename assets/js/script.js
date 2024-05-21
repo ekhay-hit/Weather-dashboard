f5764889270b03012e1af27e799cc33af7ba832e
ekhay <ekhay1@outlook.com>
2024-05-21 10:17:00 -0400

    fetched data of 5 days and used for loop to pull data from the object, printed all data successfully and daynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ searchbtn.addEventListener("click", function () {
   console.log(city);
 
   const urlCordinate = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${myAPIKey}&units=imperial`;
-//   const url =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
+  // const url =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
 
   let cord = {};
 
@@ -23,14 +23,13 @@ searchbtn.addEventListener("click", function () {
     .then(function (data) {
       console.log("I am here in cordinate");
       console.log(data);
-      cord = {
-        lat: data[0].lat,
-        lon: data[0].lon,
-      };
-      console.log(cord.lat);
-  
-
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${cord.lat}&lon=${cord.lon}&appid=${myAPIKey}&units=imperial`;
+      
+      let lat =data[0].lat;
+      let lon = data[0].lon;
+      console.log(lon);
+      console.log(lat);
+        const url =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${myAPIKey}&units=imperial`
+  // const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${myAPIKey}&units=imperial`;
 
   fetch(url)
     .then(function (response) {
@@ -43,15 +42,40 @@ searchbtn.addEventListener("click", function () {
 
       console.log("I am here at Data");
       console.log(data);
+                                                                                                        console.log(data.city.name)
+                                                                                                        console.log("This is temp");
+                                                                                                        console.log(data.list[0].main.temp);
 
-      const weather = {
-        name: data.name,
-        date: dayjs().format("MM/DD/YYYY"),
-        // icon: data.weather[0].icon,
-        temp: data.main.temp,
-        wind: data.wind.speed,
-        humidity: data.main.humidity,
+      let weather = {
+        name: data.city.name,
+        // date: dayjs().format("MM/DD/YYYY"),
+        date : data.list[0].dt_txt,
+        icon: data.list[0].weather[0].icon,
+        temp: data.list[0].main.temp,
+        wind: data.list[0].wind.speed,
+        humidity: data.list[0].main.humidity,      
+        // localStorage.setItem("weather", JSON.stringify(weatherData));
       };
+      weatherData.push(weather);
+      let i =39;
+      while(i >0){
+        weather = {
+          // date: dayjs().format("MM/DD/YYYY"),
+          date : data.list[i].dt_txt,
+          // icon: data.list[i].weather[i].icon,
+          temp: data.list[i].main.temp,
+          wind: data.list[i].wind.speed,
+          humidity: data.list[i].main.humidity,      
+          // localStorage.setItem("weather", JSON.stringify(weatherData));
+        };
+        console.log(i);
+        weatherData.push(weather);
+        i=i-8;
+        
+      }
+
+      console.log("I am here after finishing the array");
+      console.log(weatherData);
 
       const mainDiv = document.createElement("div");
       mainDiv.setAttribute("class", "col-9 justify-content-center");
@@ -64,19 +88,19 @@ searchbtn.addEventListener("click", function () {
       mainDiv.append(secondDiv);
 
       let cityName = document.createElement("h3");
-      cityName.textContent = `${weather.name} ${weather.date}`;
+      cityName.textContent = `${weatherData[0].name} ${weatherData[0].date}`;
       secondDiv.append(cityName);
 
       let temp = document.createElement("p");
-      temp.textContent = `Temp: ${weather.temp} F`;
+      temp.textContent = `Temp: ${weatherData[0].temp} F`;
       secondDiv.append(temp);
 
       let wind = document.createElement("p");
-      wind.textContent = `Wind: ${weather.wind} MPH`;
+      wind.textContent = `Wind: ${weatherData[0].wind} MPH`;
       secondDiv.append(wind);
 
       let humidity = document.createElement("p");
-      humidity.textContent = `Humidity: ${weather.humidity} %`;
+      humidity.textContent = `Humidity: ${weatherData[0].humidity} %`;
       secondDiv.append(humidity);
       // appending mainDiv to weatherContainer
       weatherContainer.append(mainDiv);
@@ -95,32 +119,30 @@ searchbtn.addEventListener("click", function () {
       );
       mainDiv.append(fiveDaysContainer);
 
-      for (let i = 0; i < 5; i++) {
+      for (let i = 5; i > 0; i--) {
         let dailyDiv = document.createElement("div");
         dailyDiv.setAttribute("class", "col-2 p-3 bg-dark");
 
         cityName = document.createElement("h5");
-        cityName.textContent = `${weather.date}`;
+        cityName.textContent = `${weatherData[i].date}`;
         dailyDiv.append(cityName);
 
         temp = document.createElement("p");
-        temp.textContent = `Temp: ${weather.temp} F`;
+        temp.textContent = `Temp: ${weatherData[i].temp} F`;
         dailyDiv.append(temp);
 
         wind = document.createElement("p");
-        wind.textContent = `Wind: ${weather.wind} MPH`;
+        wind.textContent = `Wind: ${weatherData[i].wind} MPH`;
         dailyDiv.append(wind);
 
         humidity = document.createElement("p");
-        humidity.textContent = `Humidity: ${weather.humidity} %`;
+        humidity.textContent = `Humidity: ${weatherData[i].humidity} %`;
         dailyDiv.append(humidity);
 
         fiveDaysContainer.append(dailyDiv);
       }
 
-      weatherData.push(weather);
-      localStorage.setItem("weather", JSON.stringify(weatherData));
-      console.log(weatherData);
+    
 
       // storeData(){
 
