1785a05448b59f9b108ac0a5072ae64c9866add1
Houssine <ekhay1@outlook.com>
2024-05-22 22:35:35 -0400

    icons
     added the logic for icon, and displayed base of weather condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 const searchbtn = document.querySelector("#search-btn");
 const weatherContainer = document.querySelector(".main");
 const inputCity = document.querySelector("#city");
-
+const search = document.querySelector("search");
 const myAPIKey = "c3dce583129645e5fbfd8906c1347162";
 // const city = inputCity.value;
 
 const weatherData = [];
+// let  searchHistory;
 
 searchbtn.addEventListener("click", function () {
   let city = inputCity.value;
-  console.log(city);
+
+  // searchHistory.push(city);
+  // localStorage.setItem("city",JSON.stringify(searchHistory))
+  // console.log(city);
 
   const urlCordinate = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${myAPIKey}&units=imperial`;
   // const url =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
@@ -23,127 +27,135 @@ searchbtn.addEventListener("click", function () {
     .then(function (data) {
       console.log("I am here in cordinate");
       console.log(data);
-      
-      let lat =data[0].lat;
+
+      let lat = data[0].lat;
       let lon = data[0].lon;
       console.log(lon);
       console.log(lat);
-        const url =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${myAPIKey}&units=imperial`
-  // const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${myAPIKey}&units=imperial`;
+      const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${myAPIKey}&units=imperial`;
 
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      if (inputCity.value == "") {
-        alert("Enter the city name");
-      }
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          if (inputCity.value == "") {
+            alert("Enter the city name");
+          }
 
-      console.log("I am here at Data");
-      console.log(data);
+          console.log("I am here at Data");
+          console.log(data);
 
-      let weather = {
-        name: data.city.name,
-        // date: dayjs().format("MM/DD/YYYY"),
-        date : data.list[0].dt_txt,
-        icon: data.list[0].weather[0].icon,
-        temp: data.list[0].main.temp,
-        wind: data.list[0].wind.speed,
-        humidity: data.list[0].main.humidity,      
-        // localStorage.setItem("weather", JSON.stringify(weatherData));
-      };
-      weatherData.push(weather);
-      let i =39;
-      while(i >0){
-        weather = {
-          // date: dayjs().format("MM/DD/YYYY"),
-          date : data.list[i].dt_txt,
-          // icon: data.list[i].weather[i].icon,
-          temp: data.list[i].main.temp,
-          wind: data.list[i].wind.speed,
-          humidity: data.list[i].main.humidity,      
-          // localStorage.setItem("weather", JSON.stringify(weatherData));
-        };
-        console.log(i);
-        weatherData.push(weather);
-        i=i-8;
-        
-      }
+          let weather = {
+            name: data.city.name,
+            // date: dayjs().format("MM/DD/YYYY"),
+            date: data.list[0].dt_txt,
+            icon: data.list[0].weather[0].icon,
+            temp: data.list[0].main.temp,
+            wind: data.list[0].wind.speed,
+            humidity: data.list[0].main.humidity,
+            // localStorage.setItem("weather", JSON.stringify(weatherData));
+          };
+          weatherData.push(weather);
+          let i = 39;
+          while (i > 0) {
+            weather = {
+              // JSON.stringify(json.weather[0].icon);
+              // date: dayjs().format("MM/DD/YYYY"),
+              date: data.list[i].dt_txt,
+              icon: data.list[i].weather[0].icon,
+              temp: data.list[i].main.temp,
+              wind: data.list[i].wind.speed,
+              humidity: data.list[i].main.humidity,
+              // localStorage.setItem("weather", JSON.stringify(weatherData));
+            };
+            weatherData.push(weather);
+            i = i - 8;
+          }
 
-      console.log("I am here after finishing the array");
-      console.log(weatherData);
+          console.log("I am here after finishing the array");
+          console.log(weatherData);
 
-      const mainDiv = document.createElement("div");
-      mainDiv.setAttribute("class", "col-9 justify-content-center");
-      //****************** */
-      const secondDiv = document.createElement("div");
-      secondDiv.setAttribute(
-        "class",
-        "row h-25 bg-white shadow p-3 rounded m-3"
-      );
-      mainDiv.append(secondDiv);
+          const mainDiv = document.createElement("div");
+          mainDiv.setAttribute("class", "col-9 justify-content-center");
+          //****************** */
+          const secondDiv = document.createElement("div");
+          secondDiv.setAttribute(
+            "class",
+            "row h-25 bg-danger shadow p-3 rounded m-3"
+          );
+          mainDiv.append(secondDiv);
 
-      let cityName = document.createElement("h3");
-      cityName.textContent = `${weatherData[0].name} ${weatherData[0].date}`;
-      secondDiv.append(cityName);
+          let cityName = document.createElement("h3");
+          cityName.textContent = `${weatherData[0].name} ${weatherData[0].date}`;
+          secondDiv.append(cityName);
 
-      let temp = document.createElement("p");
-      temp.textContent = `Temp: ${weatherData[0].temp} F`;
-      secondDiv.append(temp);
+          let icon = document.createElement("img");
+          icon.setAttribute('src',`https://openweathermap.org/img/w/${weatherData[0].icon}.png`)
+          icon.setAttribute('id', 'todayIcon');
+          icon.textContent = `Icon: ${weatherData[0].icon}`;
+          secondDiv.append(icon);
 
-      let wind = document.createElement("p");
-      wind.textContent = `Wind: ${weatherData[0].wind} MPH`;
-      secondDiv.append(wind);
+          let temp = document.createElement("p");
+          temp.textContent = `Temp: ${weatherData[0].temp} F`;
+          secondDiv.append(temp);
 
-      let humidity = document.createElement("p");
-      humidity.textContent = `Humidity: ${weatherData[0].humidity} %`;
-      secondDiv.append(humidity);
-      // appending mainDiv to weatherContainer
-      weatherContainer.append(mainDiv);
+          let wind = document.createElement("p");
+          wind.textContent = `Wind: ${weatherData[0].wind} MPH`;
+          secondDiv.append(wind);
 
-      // 5 day implementation
+          let humidity = document.createElement("p");
+          humidity.textContent = `Humidity: ${weatherData[0].humidity} %`;
+          secondDiv.append(humidity);
 
-      const fiveDaysH3 = document.createElement("h3");
-      fiveDaysH3.setAttribute("class", "m-4");
-      fiveDaysH3.textContent = `5-Days Forecase:`;
-      mainDiv.append(fiveDaysH3);
+          
+          // appending mainDiv to weatherContainer
+          weatherContainer.append(mainDiv);
 
-      const fiveDaysContainer = document.createElement("div");
-      fiveDaysContainer.setAttribute(
-        "class",
-        "row h-25  gap-4 text-white justify-content-center"
-      );
-      mainDiv.append(fiveDaysContainer);
+          // 5 day implementation
 
-      for (let i = 5; i > 0; i--) {
-        let dailyDiv = document.createElement("div");
-        dailyDiv.setAttribute("class", "col-2 p-3 bg-dark");
+          const fiveDaysH3 = document.createElement("h3");
+          fiveDaysH3.setAttribute("class", "m-4");
+          fiveDaysH3.textContent = `5-Days Forecase:`;
+          mainDiv.append(fiveDaysH3);
 
-        cityName = document.createElement("h5");
-        cityName.textContent = `${weatherData[i].date}`;
-        dailyDiv.append(cityName);
+          const fiveDaysContainer = document.createElement("div");
+          fiveDaysContainer.setAttribute(
+            "class",
+            "row h-25  gap-4 text-white justify-content-center"
+          );
+          mainDiv.append(fiveDaysContainer);
 
-        temp = document.createElement("p");
-        temp.textContent = `Temp: ${weatherData[i].temp} F`;
-        dailyDiv.append(temp);
+          for (let i = 5; i > 0; i--) {
+            let dailyDiv = document.createElement("div");
+            dailyDiv.setAttribute("class", "col-2 p-3 h-75 bg-dark");
 
-        wind = document.createElement("p");
-        wind.textContent = `Wind: ${weatherData[i].wind} MPH`;
-        dailyDiv.append(wind);
+            cityName = document.createElement("h5");
+            cityName.textContent = `${weatherData[i].date}`;
+            dailyDiv.append(cityName);
 
-        humidity = document.createElement("p");
-        humidity.textContent = `Humidity: ${weatherData[i].humidity} %`;
-        dailyDiv.append(humidity);
+            temp = document.createElement("p");
+            temp.textContent = `Temp: ${weatherData[i].temp} F`;
+            dailyDiv.append(temp);
 
-        fiveDaysContainer.append(dailyDiv);
-      }
+            wind = document.createElement("p");
+            wind.textContent = `Wind: ${weatherData[i].wind} MPH`;
+            dailyDiv.append(wind);
 
-    
+            humidity = document.createElement("p");
+            humidity.textContent = `Humidity: ${weatherData[i].humidity} %`;
+            dailyDiv.append(humidity);
+         icon = document.createElement("img");
+          icon.setAttribute('src',`https://openweathermap.org/img/w/${weatherData[i].icon}.png`)
+          icon.textContent = `Icon: ${weatherData[0].icon}`;
+          dailyDiv.append(icon);
 
-      // storeData(){
+            fiveDaysContainer.append(dailyDiv);
+          }
 
-      // }
+          // storeData(){
+
+          // }
+        });
     });
-});
 });
